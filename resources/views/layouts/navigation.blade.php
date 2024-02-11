d313c250c51cdac559c06d045ce3f074ba8fdbc9
Terry Wambui <shikuwambui@gmail.com>
2024-02-11 22:46:55 +0300

    Model, Controllers and Migration

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    {{-- add a menu item for desktop screens --}}
+                   <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                    {{ __('Chirps') }}
+                </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +74,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                {{ __('Chirps') }}
             </x-responsive-nav-link>
         </div>
 
